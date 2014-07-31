23bc136c17e7395d5c3e7e1560f018b78c95d993
James F Wilkus <jfwilkus@mac.com>
2014-07-31 17:18:42 -0500

    install packages on each platform

---
+++ recipes/packages.rb
@@ -0,0 +1,13 @@
+
+if platform_family?('mac_os_x')
+  include_recipe 'homebrew'
+end
+
+if platform_family?('rhel')
+  include_recipe "yum"
+  include_recipe "yum-epel"
+
+  node[:yum][:packages].each do |package|
+    yum_package package
+  end
+end
