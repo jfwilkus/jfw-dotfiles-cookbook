5544e6443b777de812ee68acaf994c54c9724c5f
James F Wilkus <jfwilkus@mac.com>
2014-07-31 17:18:12 -0500

    initial attributes

---
+++ attributes/default.rb
@@ -0,0 +1,16 @@
+default[:homebrew][:formulas] = [
+  'bash-completion',
+  'rbenv',
+  'ruby-build',
+  'git',
+  'git-flow',
+  'packer',
+  'gnupg',
+  'go',
+  'vagrant-completion'
+]
+default[:yum][:packages] = [
+  'git',
+  'bash-completion',
+  'docker-io'
+]
