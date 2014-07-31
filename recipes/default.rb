73a90a22aa71e9b72c23319515b34397ef94fd1f
James F Wilkus <jfwilkus@mac.com>
2014-07-31 15:07:12 -0500

    add remote_file for .bashrc

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,14 @@
 # Cookbook Name:: jfw-dotfiles-cookbook
 # Recipe:: default
 #
-# Copyright (C) 2014 
+# Copyright (C) 2014
 #
-# 
 #
+#
+
+remote_file "#{node['user']['home']}/.bashrc" do
+  mode "0700"
+  source "https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.bashrc"
+  owner node['user']['name']
+  owner node['user']['group']
+end
