f0bba90d01d43d5c4873e9d9ae284d70ec73f0da
James F Wilkus <jfwilkus@mac.com>
2014-07-31 17:40:07 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jfwilkus@mac.com'
 license          ''
 description      'Installs/Configures jfw-dotfiles-cookbook'
 long_description 'Installs/Configures jfw-dotfiles-cookbook'
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'homebrew'
 depends 'yum'
