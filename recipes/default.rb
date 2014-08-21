2a268ec8337a2bf566f0081b27a4d911f6fb4a09
James F Wilkus <jfwilkus@mac.com>
2014-08-21 06:46:56 -0500

    move to minifu::home

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,58 +7,6 @@
 #
 #
 
-remote_file "#{node['user']['home']}/.bashrc" do
-  mode '0755'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.bashrc'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.bash_profile" do
-  mode '0755'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.bash_profile'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.editorconfig" do
-  mode '0644'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.editorconfig'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.gemrc" do
-  mode '0644'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.gemrc'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.perltidyrc" do
-  mode '0644'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.perltidyrc'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.profile" do
-  mode '0755'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.profile'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-template "#{node['user']['home']}/.gitconfig" do
-  source 'gitconfig.erb'
-  mode '0644'
-  owner node['user']['name']
-  group node['user']['group']
-end
-
-remote_file "#{node['user']['home']}/.vimrc" do
-  mode '0644'
-  source 'https://raw.githubusercontent.com/jfwilkus/dotfiles/master/.vimrc'
-  owner node['user']['name']
-  group node['user']['group']
-end
+include_recipe 'minifu::home'
+include_recipe 'minifu::sudoers'
+include_recipe 'minifu::packages'
