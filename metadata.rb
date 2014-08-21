1078d662581bc5adc10acb0733c9ef2d10ec6ba8
James F Wilkus <jfwilkus@mac.com>
2014-08-21 07:03:32 -0500

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jfwilkus@mac.com'
 license          ''
 description      'Installs/Configures my personal systems'
 long_description 'Installs/Configures my personal systems'
-version          '0.1.15'
+version          '0.1.16'
 
 depends 'homebrew'
 depends 'yum'
